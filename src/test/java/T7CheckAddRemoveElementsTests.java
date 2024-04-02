e6cb5ae046974154fcf38a14d03fe16ed398f435
nastiykr <nastiykr@gmail.com>
2024-04-02 18:35:54 +0300

    add new test

--- src/test/java/T7CheckAddRemoveElementsTests.java
+++ src/test/java/T7CheckAddRemoveElementsTests.java
@@ -1,18 +1,26 @@
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.TestFactory;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Stream;
 
+import static com.codeborne.selenide.CollectionCondition.size;
+import static com.codeborne.selenide.Condition.empty;
+import static com.codeborne.selenide.Condition.value;
 import static com.codeborne.selenide.Selectors.byClassName;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class T7CheckAddRemoveElementsTests extends BaseTest{
+
+    SelenideElement addElementButton = $(byText("Add Element"));
+    ElementsCollection deleteButtons = $$(byClassName("added-manually"));
 
     /**
      * Перейти на страницу Add/Remove Elements.
@@ -24,39 +32,71 @@ public class T7CheckAddRemoveElementsTests extends BaseTest{
      */
     @TestFactory
     @DisplayName("Check inputs positive cases")
-    public void test7CheckAddRemoveElements() {
-
-        SelenideElement addElementButton = $(byText("Add Element"));
-        ElementsCollection deleteButtons = $$(byClassName("added-manually"));
+    public List<DynamicTest> test7CheckAddRemoveElements() {
 
         open(BASE_URL + "add_remove_elements/");
 
-        int attempts = 0;
+        List<DynamicTest> result = new ArrayList<>();
+        List<Integer> countClickAddList = countClickAddList();
+        List<Integer> countClickDeleteList = countClickDeleteList();
 
-        while (attempts < 5) {
-            addElementButton.click();
-            attempts++;
-        }
 
-        Random rand = new Random();
-
-        for (int i = 0; i < 3; i++) {
-            $(byClassName("added-manually"), rand.nextInt(attempts - i)).click();
-
-            deleteButtons.forEach(deleteButton -> {
-                System.out.println("Text for picture: " + deleteButton.getText());
-            });
-        }
-    }
-
-    private List<String> testDataCollectorForPositiveCases(int size) {
-        Random rand = new Random();
-        List<String> result = new ArrayList<>();
-
-        for (int i = 0; i < size; i++) {
-            result.add(String.valueOf(rand.nextInt(10000) + 1));
+        for (int i = 0; i < countClickAddList.size(); i++) {
+            final int index = i;
+            result.add(
+                    DynamicTest.dynamicTest(
+                            "Dynamic test #" + i,
+                            () -> {
+                                System.out.println("count click add - " + countClickAddList.get(index));
+                                System.out.println("------------------------------------------------------ ");
+                                clickAddButton(countClickAddList.get(index));
+                                System.out.println("------------------------------------------------------ ");
+                                System.out.println("count click delete - " + countClickDeleteList.get(index));
+                                System.out.println("------------------------------------------------------ ");
+                                clickDeleteButton(countClickAddList.get(index), countClickDeleteList.get(index));
+                            }
+                    )
+            );
         }
 
         return result;
+    }
+
+    private List<Integer> countClickAddList() {
+        List<Integer> result = new ArrayList<>();
+
+        result.add(2);
+        result.add(5);
+        result.add(1);
+
+        return result;
+    }
+
+    private List<Integer> countClickDeleteList() {
+        List<Integer> result = new ArrayList<>();
+
+        result.add(1);
+        result.add(2);
+        result.add(3);
+
+        return result;
+    }
+
+
+    private void clickAddButton(int countClickAdd) {
+        for (int i = 1; i <= countClickAdd; i++) {
+            System.out.println("click add number - " + i);
+            addElementButton.click();
+        }
+        deleteButtons.should(size(countClickAdd));
+    }
+
+    private void clickDeleteButton(int countClickAdd, int countClickDelete) {
+        for (int i = 1; i <= countClickDelete; i++) {
+            Random rand = new Random();
+            System.out.println("click delete number - " + i);
+            deleteButtons.get(rand.nextInt(deleteButtons.size() - i)).click();
+        }
+        deleteButtons.should(size(countClickAdd - countClickDelete));
     }
 }
